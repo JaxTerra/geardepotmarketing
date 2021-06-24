f1a8bf2cc7b7d9836667aa0dbe06415bbaa74ba2
JaxTerra <jax@beitsafe.com>
2021-06-24 13:23:46 +1000

    ADD - recaptcha

--- resources/views/site/partials/contact.blade.php
+++ resources/views/site/partials/contact.blade.php
@@ -168,6 +168,9 @@
                                           aria-describedby="message-max"></textarea>
                             </div>
                         </div>
+
+                        @captcha
+
                         <div class="sm:col-span-2 sm:flex sm:justify-end">
                             <button type="submit" class="mt-2 w-full inline-flex items-center justify-center px-6 py-3 border border-transparent rounded-md shadow-sm text-base font-medium text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 sm:w-auto">
                                 Submit
